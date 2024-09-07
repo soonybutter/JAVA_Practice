fcde842e37e782994b280a30ccb7cce597154073
Yang Da Yeon <ydy229@naver.com>
2024-09-07 22:11:09 +0900

    [JAVA IO 추가 학습] 제목: 테스트용 입니다.

--- src/com/tistory/practice01/ForTest01.java
+++ src/com/tistory/practice01/ForTest01.java
@@ -1,9 +1,9 @@
 package com.tistory.practice01;
 
-public class ForTest {
+public class ForTest01 {
 
 	public static void main(String[] args) {
-		System.out.println("Hi");
+		System.out.println("hi hi hi");
 
 	}
 
